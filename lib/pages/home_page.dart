b4d143ef67562ece0b8070688ba6b2b33139a4b5
Ramdhas <ramdhas@MacBook-Pro-som-tillhor-Ramdhas.local>
2023-09-25 14:13:20 +0200

    Initial commit with list view in Homepage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,58 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:my_todo/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Do Exercise", true]
+  ];
+
+  void checkBoxChanged(bool? value, int index){
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void createNewTask() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog();
+        },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text('TODO'),
+        elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder (
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+              taskName: toDoList[index][0],
+              taskCompleted: toDoList[index][1],
+              onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
+      ),
+    );
+  }
+}
